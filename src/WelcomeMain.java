5626615b0cd6dd204bb62bbb53eda97eb6191df8
Agnieszka Majak <agamajak@wp.pl>
2020-04-22 20:19:20 +0200

    created package logic, added specifiers

--- src/WelcomeMain.java
+++ src/WelcomeMain.java
@@ -1,3 +1,5 @@
+import logic.WelcomeLogic;
+
 class WelcomeMain {
     public static void main(String[] args) {
 
--- src/logic/WelcomeLogic.java
+++ src/logic/WelcomeLogic.java
@@ -1,18 +1,21 @@
+package logic;
+
 import java.util.Scanner;
 
-class WelcomeLogic {
-    String[] names = new String[5];
+public class WelcomeLogic {
+    private final String[] names = new String[5];
 
-    void addNamesFromUser() {
+    public void addNamesFromUser() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Podaj 5 imion:");
         for (int i = 0; names.length > i; i++) {
             String name = scan.nextLine();
             names[i] = name;
         }
+        scan.close();
     }
 
-    void returnWelcomes() {
+    public void returnWelcomes() {
         for (int i = names.length - 1; i < names.length && i >= 0; i--) {
             System.out.println("Cześć " + names[i]);
         }
